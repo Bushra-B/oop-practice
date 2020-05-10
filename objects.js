e264367bcfd884b32098f59fd07032e0ee23ea07
Bushra-B <bushraxbil@gmail.com>
2020-05-10 15:46:56 +0300

    classes

--- objects.js
+++ objects.js
@@ -1,55 +1,10 @@
 'use strict';
 
-//creating an obj. using object literals :- 
+//creating objects using ES6 classes :- 
 /*** 
- var obj_name = {
-    property_name: proprety_value,
-    .
-    .
-    .
-    method_name(){
-        function code;
-    },
+ class Class_name = {
+    
 }  
 ***/
-var userOne = {
-    email: 'bushra@robi.com',
-    name: 'Bushra',
-    login(){
-        console.log(this.email, 'has logged in');
-    },
-    logout(){
-        console.log(this.email, 'has logged out');
-    }
-};
-
-console.log(userOne.name);
-
-////////////////////////////////////////
-//REM: Updating Object Properties//
-/*** 
-#Method One: Dot Notation :- 
-``obj_name.property_name = new_property_vlaue;``
-=> userOne.name = 'updated name';
-
-#Method Two: Using Square Brackets :- 
-``obj_name['property_name'] = new_property_value;``
-=> userOne['email'] = 'updated email';
-***/
-//Method Two is useful to access dynamic properties because you can pass a variable in the [] for the property.
-/////////////////////////////////////////
-
-////////////////////////////////////////
-//REM: Creating New Object Properties and Methods//
-/*** 
-#A new obj. property can be added after the obj. is created by :-
-``obj_name.new_property_name = property_vlaue;``
-=> userOne.age = 24;
-
-#A new obj. mathod can be added after the obj. is created by :-
-``obj_name.method_name = function(){function code};``
-=> userOne.logInfo = function(){//any code here};
-***/
-/////////////////////////////////////////
 
 
