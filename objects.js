9ee20d1a78799e465e2b253225f3281111eb957a
Bushra-B <bushraxbil@gmail.com>
2020-05-10 16:53:01 +0300

    method chaining

--- objects.js
+++ objects.js
@@ -24,17 +24,38 @@ class User {
     constructor(name, email){
         this.name = name;
         this.email = email;
+        this.score = 0;
     };
     login(){
         console.log(this.email, 'just logged in');
+        return this;
     };
     logout(){
         console.log(this.email, 'just logged out');
+        return this;
     };
+    updateScore(){
+        this.score++;
+        console.log(this.name, 'now has a score of ', this.score);
+        return this;
+    }
 };
 
 let userOne = new User('Bushra', 'bushra@robi.com');
 
 console.log('userOne', userOne);
-userOne.login(); userOne.logout();
+// userOne.login(); userOne.logout(); //the regular way of calling methods on an object
+userOne.login().updateScore().updateScore().updateScore().logout();
+
+
+////////////////////////////////////////
+//NOTE: Using Method Chaining//
+/*** 
+#Method Chaining: 
+- with method chaining we can call several methods on an object in one line of code useing the dot notation.
+
+- to be able to do that we need to return the object (this) after each method so that we can call the other
+  methods on that same object.   --without doing this we would be calling the other methods on (undefined)--
+***/
+/////////////////////////////////////////
 
