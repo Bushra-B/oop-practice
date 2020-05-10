917810df4dd4b9820f60f6446e9cbcf368e6d7a3
Bushra-B <bushraxbil@gmail.com>
2020-05-10 16:08:28 +0300

    class constructor

--- objects.js
+++ objects.js
@@ -2,9 +2,28 @@
 
 //creating objects using ES6 classes :- 
 /*** 
- class Class_name = {
-    
+ class Class_name {
+    constructor(argument1, argument2, ...){
+        this.property1_name = argument1;
+        this.property2_name = argument2;
+        .
+        .
+        .
+    }
 }  
 ***/
+/*** 
+ var obj_name = new Class_name(argument1_value, argument2_value, ...); 
+***/
 
+class User {
+    constructor(name, email){
+        this.name = name;
+        this.email = email;
+    };
+};
+
+let userOne = new User('Bushra', 'bushra@robi.com');
+
+console.log('userOne', userOne);
 
